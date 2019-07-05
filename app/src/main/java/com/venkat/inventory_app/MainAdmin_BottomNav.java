4177a3f13f8a4f6903bff5e8be6d080791e9561c
adityap301 <adityap301@gmail.com>
2019-07-05 14:19:19 +0530

    Log Out menu added.

--- app/src/main/java/com/venkat/inventory_app/MainAdmin_BottomNav.java
+++ app/src/main/java/com/venkat/inventory_app/MainAdmin_BottomNav.java
@@ -1,13 +1,21 @@
 package com.venkat.inventory_app;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.venkat.inventory_app.Admin.Admin_Request_Frag;
+import com.venkat.inventory_app.Common.MainActivity;
 
 public class MainAdmin_BottomNav extends AppCompatActivity {
 
@@ -20,6 +28,35 @@ public class MainAdmin_BottomNav extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Admin_available_frag()).commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout_menu_item:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // user is now signed out
+                                startActivity(new Intent( MainAdmin_BottomNav.this, MainActivity.class));
+                                Log.d("AUTH","USER LOGGED OUT");
+                                finish();
+                            }
+                        });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener=
--- app/src/main/java/com/venkat/inventory_app/User/MainUser_BottomNav.java
+++ app/src/main/java/com/venkat/inventory_app/User/MainUser_BottomNav.java
@@ -1,14 +1,22 @@
 package com.venkat.inventory_app.User;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.venkat.inventory_app.Admin_available_frag;
+import com.venkat.inventory_app.Common.MainActivity;
 import com.venkat.inventory_app.R;
 
 public class MainUser_BottomNav extends AppCompatActivity {
@@ -20,6 +28,36 @@ public class MainUser_BottomNav extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.user_bottom_nav,new User_Available_Frag()).commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout_menu_item:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // user is now signed out
+                                startActivity(new Intent( MainUser_BottomNav.this, MainActivity.class));
+                                Log.d("AUTH","USER LOGGED OUT");
+                                finish();
+                            }
+                        });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 
