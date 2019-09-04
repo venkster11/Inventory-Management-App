0d273d5464200a9b66eec83e4ee146b10c28b616
venkster11 <venkateshm11799@gmail.com>
2019-09-04 18:42:58 +0530

    Login Fix

--- app/src/main/java/com/venkat/inventory_app/Common/MainActivity.java
+++ app/src/main/java/com/venkat/inventory_app/Common/MainActivity.java
@@ -1,26 +1,42 @@
 package com.venkat.inventory_app.Common;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.CircularProgressDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.venkat.inventory_app.Not_needed.MainADM;
 import com.venkat.inventory_app.Admin.MainAdmin_BottomNav;
 import com.venkat.inventory_app.R;
 import com.venkat.inventory_app.User.MainUser_BottomNav;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,82 +44,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int RC_SIGN_IN = 123;
     String userid;
 
+    final FirebaseFirestore db=FirebaseFirestore.getInstance();
+    FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+    ProgressDialog dialog;
+
+    final DocumentReference admref = db.collection("Admins").document("admin_id");
+    //final CollectionReference admref = db.collection("Admins");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //  FirebaseApp.initializeApp(this);
 
 
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Please Wait");
+        dialog.setMessage("We're logging you in");
 
         auth=FirebaseAuth.getInstance();
+
+        // If logged in already, login to Inventory
+        // Else, set listener to button, clicking it would launch the Sign-In activity
         if(auth.getCurrentUser()!=null){
-            //user already signed in
-            Log.d("AUTH",auth.getCurrentUser().getEmail());
-
-            FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
-            userid=currentFirebaseUser.getEmail();
-
-            if( userid.equals("venkateshm11799@gmail.com") || userid.equals("adityap301@gmail.com")) {
-                Toast.makeText(this, "ADMIN - " + currentFirebaseUser.getEmail(), Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(MainActivity.this, MainAdmin_BottomNav.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                //startActivity(intent);
-                finish();
-            }
-            else
-            {
-
-                Intent intent1=new Intent(MainActivity.this, MainUser_BottomNav.class);
-                startActivity(intent1);
-                intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                finish();
-                Toast.makeText(this, "USER - " + userid, Toast.LENGTH_SHORT).show();
-            }
-        }
-        else {
-            startActivityForResult(AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(Arrays.asList(
-                                    new AuthUI.IdpConfig.GoogleBuilder().build())).build(),
-                    RC_SIGN_IN
-            );
-
+            loginToInventory();
+        } else {
+            findViewById(R.id.btnlogout).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivityForResult(AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build())).build(),
+                            RC_SIGN_IN
+                    );
+                    dialog.show();
+                }
+            });
         }
 
-        findViewById(R.id.btnlogout).setOnClickListener(this);
+    }
 
-
-       /* Button B_Nav=(Button)findViewById(R.id.btn_nav);
-        Button btnadmin=(Button)findViewById(R.id.admin);
-        Button btnuser=(Button)findViewById(R.id.user);
-
-        B_Nav.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent3=new Intent(MainActivity.this, MainAdmin_BottomNav.class);
-                startActivity(intent3);
-            }
-        });
-
-        btnadmin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, MainADM.class);
-                startActivity(intent);
-
-            }
-        });
-
-        btnuser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent1=new Intent(MainActivity.this, MainUser_BottomNav.class);
-                startActivity(intent1);
-            }
-        });*/
-
+    private void loginToInventory() {
+        dialog.show();
+        db.collection("Admins").whereEqualTo(FieldPath.documentId(), FirebaseAuth.getInstance().getCurrentUser().getEmail())
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                        // If the Snapshot Object is null, we've received some valid data
+                        if (queryDocumentSnapshots != null) {
+                            // If the size is 1, then the document exists, which proves that
+                            // User is admin
+                            if (queryDocumentSnapshots.size() == 1) {
+                                Intent intent=new Intent(MainActivity.this, MainAdmin_BottomNav.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                finish();
+                            } else if (queryDocumentSnapshots.size() == 0) {
+                                // That condition was just to be sure, hehe
+                                Intent intent1=new Intent(MainActivity.this, MainUser_BottomNav.class);
+                                startActivity(intent1);
+                                intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                finish();
+                            }
+                            dialog.dismiss();
+                        }
+                    }
+                });
     }
 
     @Override
@@ -128,7 +134,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(requestCode==RC_SIGN_IN){
             if(resultCode==RESULT_OK){
                 Log.d("AUTH",auth.getCurrentUser().getEmail());
-
+                // Log in successful, we'll proceed to Inventory Log in
+                loginToInventory();
             }
             else {
                 Log.d("AUTH","NOT AUTHENTICATED");
